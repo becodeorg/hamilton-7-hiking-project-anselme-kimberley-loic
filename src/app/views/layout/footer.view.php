35737d58754b0e102a55d6f5d08a5596fd901709
Anselme Dor <anselmedor@hotmail.be>
2022-12-20 11:53:06 +0100

    Responsive navbar w/ JS, custom tailwind.config

--- src/app/views/layout/footer.view.php
+++ src/app/views/layout/footer.view.php
@@ -1,16 +1,10 @@
 </body>
-<footer class="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
-    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.
+<footer class="p-4 bg-white rounded-lg shadow flex flex-col md:flex-row items-center md:justify-between md:p-6">
+    <span class="text-sm text-gray-500 sm:text-center">© 2022 BeCode</a>. All Rights Reserved.
     </span>
-    <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+    <ul class="flex flex-wrap  mt-3 text-sm dark:text-gray-400 sm:mt-0">
         <li>
-            <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
-        </li>
-        <li>
-            <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
-        </li>
-        <li>
-            <a href="#" class="mr-4 hover:underline md:mr-6">Licensing</a>
+            <a href="#" class="mr-4 hover:underline md:mr-6">About</a>
         </li>
         <li>
             <a href="#" class="hover:underline">Contact</a>
--- src/public/index.php
+++ src/public/index.php
@@ -3,4 +3,3 @@ declare(strict_types=1);
 require_once("app/views/layout/head.view.php");
 require_once("app/views/Hikes.view.php");
 require_once("app/views/layout/footer.view.php");
-
